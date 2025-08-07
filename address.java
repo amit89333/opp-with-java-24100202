729bd563fd8e3fa0d7e2e9fd561bb7a2d2755485
amit89333 <amityadav955656@gmail.com>
2025-08-07 23:16:15 +0530

    Create address.java

---
+++ address.java
@@ -0,0 +1,15 @@
+public class PrintAddress {
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Block A \nBeta 1\nGreater Noida\nU.P.");
+        
+        System.out.println();
+
+   
+        System.out.println("Block A");
+        System.out.println("Beta 1");
+        System.out.println("Greater Noida");
+        System.out.println("U.P.");
+    }
+}
